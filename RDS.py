144f990cc13cbcb4799cf0623f29494c03c3633e
alexzhao428 <ruyangzhao2022@u.northwestern.edu>
2022-05-06 00:21:21 -0500

    Add logger

--- RDS.py
+++ RDS.py
@@ -1,5 +1,10 @@
-# import os
+import logging.config
 from sqlalchemy import create_engine
+
+
+# Create a logger and specify the configurations
+logging.config.fileConfig('./config/logging/local.conf')
+logger = logging.getLogger('RDS.py')
 
 ENGINE_STRING = "mysql+pymysql://rzx9163:Aws1998!@nw-msia423-rzx9163.ctarvegaqgdp.us-east-1.rds.amazonaws.com:3306/msia423_db"
 engine = create_engine(ENGINE_STRING)
