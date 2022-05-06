1941a3acee7d1bcc36ff6c2d9f8b6b999f1b36f5
alexzhao428 <ruyangzhao2022@u.northwestern.edu>
2022-05-05 23:39:53 -0500

    Get bucket names

--- data_upload.py
+++ data_upload.py
@@ -2,9 +2,15 @@ import boto3
 import botocore
 import logging.config
 
+
+# Create a logger and specify the configurations
 logging.config.fileConfig('./config/logging/local.conf')
 logger = logging.getLogger('data_upload.py')
-s3 = boto3.resource('s3')
+
+# s3 = boto3.resource('s3')
+# # Get all the bucket names
+# buckets = [bucket.name for bucket in s3.buckets.all()]
+
 
 try:
     s3.meta.client.upload_file('data/external/flights_fares_data.csv', '2022-msia423-zhao-ruyang', 'flights_fares.csv')
