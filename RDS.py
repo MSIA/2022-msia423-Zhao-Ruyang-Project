5c119a4fb635a751afc64508c04854e9a062c498
alexzhao428 <ruyangzhao2022@u.northwestern.edu>
2022-05-06 00:14:37 -0500

    Create RDS.py

---
+++ RDS.py
@@ -0,0 +1,26 @@
+# import os
+from sqlalchemy import create_engine
+
+ENGINE_STRING = "mysql+pymysql://rzx9163:Aws1998!@nw-msia423-rzx9163.ctarvegaqgdp.us-east-1.rds.amazonaws.com:3306/msia423_db"
+engine = create_engine(ENGINE_STRING)
+
+try:
+    with engine.connect() as con:
+        con.execute('DROP DATABASE IF EXISTS test;')
+        con.execute('CREATE DATABASE test;')
+        con.execute('USE test;')
+        con.execute("""
+        CREATE TABLE UserInputs (
+            cUserID int(11),
+            DepartureDate date,
+            DepartureTime time,
+            Source varchar(255),
+            Destination varchar(255),
+            Stops int(11),
+            TotalTime int(11),
+            FlightNumber varchar(255)
+        );
+        """
+                    )
+except Exception as e:
+    raise e
