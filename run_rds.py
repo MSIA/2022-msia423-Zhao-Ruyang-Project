c4d4d8fe1b8c0342e78c196b37ed818d4f01c5ba
alexzhao428 <ruyangzhao2022@u.northwestern.edu>
2022-05-08 17:13:18 -0500

    Update run_rds.py

--- run_rds.py
+++ run_rds.py
@@ -1,9 +1,9 @@
 import logging.config
 
-from config.flaskconfig import SQLALCHEMY_DATABASE_URI
+from config.flaskconfig import SQLALCHEMY_DATABASE_URI, LOGGING_CONFIG
 from src.sql_util import create_db
 
-logging.config.fileConfig('config/logging/local.conf')
+logging.config.fileConfig(LOGGING_CONFIG)
 logger = logging.getLogger('run_rds.py')
 
 create_db(SQLALCHEMY_DATABASE_URI)
