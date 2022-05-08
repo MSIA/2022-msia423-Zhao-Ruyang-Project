8a9f61cdf90fb4dadd3f081487113e9b9216296c
alexzhao428 <ruyangzhao2022@u.northwestern.edu>
2022-05-08 17:14:02 -0500

    Replace hardcoded path with path imported from config

--- run_s3.py
+++ run_s3.py
@@ -1,17 +1,18 @@
 import argparse
 import logging.config
 
+from config.flaskconfig import LOGGING_CONFIG, LOCAL_DATA_PATH, S3_DATA_PATH
 from src.s3_util import download_file_from_s3, upload_file_to_s3
 
-logging.config.fileConfig('config/logging/local.conf')
+logging.config.fileConfig(LOGGING_CONFIG)
 logger = logging.getLogger('run_s3.py')
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--download', default=False, action='store_true',
                     help="If True, will download the data from S3. If False, will upload data to S3")
-parser.add_argument('--s3_path', default='s3://2022-msia423-zhao-ruyang/raw/flights_fares_data.csv',
+parser.add_argument('--s3_path', default=S3_DATA_PATH,
                     help="s3 data path to download or upload data")
-parser.add_argument('--local_path', default='data/external/flights_fares_data.csv',
+parser.add_argument('--local_path', default=LOCAL_DATA_PATH,
                     help="local data path to store or upload data")
 args = parser.parse_args()
 
