281c4ad64475a46060152bf53fe04c7840256495
alexzhao428 <ruyangzhao2022@u.northwestern.edu>
2022-05-23 20:03:40 -0500

    Modify path

--- run_s3.py
+++ run_s3.py
@@ -1,7 +1,8 @@
 import argparse
 import logging.config
 
-from config.flaskconfig import LOGGING_CONFIG, LOCAL_DATA_PATH, S3_DATA_PATH
+from config.flaskconfig import LOGGING_CONFIG
+from config.model_config import RAW_DATA_PATH, S3_DATA_PATH, LOCAL_DATA_PATH
 from src.s3_util import download_file_from_s3, upload_file_to_s3
 
 logging.config.fileConfig(LOGGING_CONFIG)
@@ -12,11 +13,13 @@ parser.add_argument('--download', default=False, action='store_true',
                     help="If True, will download the data from S3. If False, will upload data to S3")
 parser.add_argument('--s3_path', default=S3_DATA_PATH,
                     help="s3 data path to download or upload data")
-parser.add_argument('--local_path', default=LOCAL_DATA_PATH,
+parser.add_argument('--upload_local_path', default=RAW_DATA_PATH,
+                    help="local data path to store or upload data")
+parser.add_argument('--download_local_path', default=LOCAL_DATA_PATH,
                     help="local data path to store or upload data")
 args = parser.parse_args()
 
 if args.download:
-    download_file_from_s3(args.local_path, args.s3_path)
+    download_file_from_s3(args.download_local_path, args.s3_path)
 else:
-    upload_file_to_s3(args.local_path, args.s3_path)
+    upload_file_to_s3(args.upload_local_path, args.s3_path)
