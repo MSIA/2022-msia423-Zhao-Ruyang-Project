b7445b688b617b3c5f88646f498d8eb85c97f6e9
alexzhao428 <ruyangzhao2022@u.northwestern.edu>
2022-05-23 20:20:49 -0500

    Add config

--- config/model_config.py
+++ config/model_config.py
@@ -3,3 +3,7 @@ RAW_DATA_PATH = "data/raw/flight_data.csv"
 S3_DATA_PATH = "s3://2022-msia423-zhao-ruyang/raw/flight_data.csv"
 LOCAL_DATA_PATH = "data/sample/flight_data.csv"
 CLEANED_DATA_PATH = "data/clean/clean_data.csv"
+
+# Preprocess config
+COLUMN_TO_MODIFY = 'stops'
+COLUMN_TO_DROP = ['flight', 'arrival_time']
