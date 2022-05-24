2e32110f0a56c798d3a36686264c484a12ce6858
alexzhao428 <ruyangzhao2022@u.northwestern.edu>
2022-05-23 20:01:54 -0500

    Create model_config.py

---
+++ config/model_config.py
@@ -0,0 +1,5 @@
+# Data path
+RAW_DATA_PATH = "data/raw/flight_data.csv"
+S3_DATA_PATH = "s3://2022-msia423-zhao-ruyang/raw/flight_data.csv"
+LOCAL_DATA_PATH = "data/sample/flight_data.csv"
+CLEANED_DATA_PATH = "data/clean/clean_data.csv"
