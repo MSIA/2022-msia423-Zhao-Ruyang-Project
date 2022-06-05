58376999366a43d20d9ef4a792546b914efa5b57
alexzhao428 <ruyangzhao2022@u.northwestern.edu>
2022-06-04 23:44:06 -0500

    Format

--- run_rds.py
+++ run_rds.py
@@ -1,14 +1,22 @@
 import logging.config
+import sqlalchemy
+from config.flaskconfig import SQLALCHEMY_DATABASE_URI
+from src.sql_util import create_db
 
-from config.flaskconfig import SQLALCHEMY_DATABASE_URI, LOGGING_CONFIG
-from src.sql_util import create_db, UserRecords
+logging.config.fileConfig('config/logging/local.conf')
+logger = logging.getLogger('create_sql_database')
 
-logging.config.fileConfig(LOGGING_CONFIG)
-logger = logging.getLogger('run_rds.py')
 
-print(SQLALCHEMY_DATABASE_URI)
-create_db(SQLALCHEMY_DATABASE_URI)
-
+try:
+    create_db(SQLALCHEMY_DATABASE_URI)
+except sqlalchemy.exc.ArgumentError as e:
+    logger.error('Could not parse URL from the engine string. %s', e)
+    raise e
+except sqlalchemy.exc.OperationalError as e:
+    logger.error('Could not establish connection. Check engine string. %s', e)
+    raise e
+else:
+    logger.info('Successfully created the tables')
 # from sqlalchemy.orm import sessionmaker
 # from sqlalchemy import create_engine
 # engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=True)
