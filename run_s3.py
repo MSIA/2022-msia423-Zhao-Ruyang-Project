875101d8d455692645d7f532d56957f2eae702c4
alexzhao428 <ruyangzhao2022@u.northwestern.edu>
2022-06-04 23:44:24 -0500

    Format

--- run_s3.py
+++ run_s3.py
@@ -10,13 +10,13 @@ logger = logging.getLogger('run_s3.py')
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--download', default=False, action='store_true',
-                    help="If True, will download the data from S3. If False, will upload data to S3")
+                    help='If True, will download the data from S3. If False, will upload data to S3')
 parser.add_argument('--s3_path', default='s3://2022-msia423-zhao-ruyang/raw/flight_data.csv',
-                    help="s3 data path to download or upload data")
+                    help='s3 data path to download or upload data')
 parser.add_argument('--upload_local_path', default='data/raw/flight_data.csv',
-                    help="local data path to store or upload data")
+                    help='local data path to store or upload data')
 parser.add_argument('--download_local_path', default='data/raw/flight_data.csv',
-                    help="local data path to store or upload data")
+                    help='local data path to store or upload data')
 args = parser.parse_args()
 
 if args.download:
--- src/app_util.py
+++ src/app_util.py
@@ -4,12 +4,13 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def count_down(X: list, days_index: int = 7) -> np.array:
-    days = int(X[days_index])
-    X = np.array(X).reshape(1, -1)
+def count_down(start_row: list, days_index: int = 7) -> np.array:
+    """Expand the list into a matrix by counting down the number of days left"""
+    days = int(start_row[days_index])
+    start_row = np.array(start_row).reshape(1, -1)
     full_matrix = None
     for day in range(days):
-        record = X.copy()
+        record = start_row.copy()
         record[0, days_index] = day
         if full_matrix is not None:
             full_matrix = np.vstack((full_matrix, record))
@@ -19,8 +20,39 @@ def count_down(X: list, days_index: int = 7) -> np.array:
     return np.array(full_matrix)
 
 
+def time_of_day(time: str):
+    """Parse hour from input str and output time of day"""
+    # Get hour from a time str and convert to int
+    hour, minute = time.split(':')
+    hour = int(hour)
+
+    # Find time of day
+    if 0 <= hour < 4:
+        segment = 'Late_Night'
+    elif 4 <= hour < 8:
+        segment = 'Early_Morning'
+    elif 9 <= hour < 12:
+        segment = 'Morning'
+    elif 13 <= hour < 16:
+        segment = 'Afternoon'
+    elif 17 <= hour < 20:
+        segment = 'Evening'
+    else:
+        segment = 'Night'
+
+    return segment
+
+
 if __name__ == '__main__':
-    a = count_down([1, 1, 2, 1, 1, 1, 1, 15])
-    print(a)
+    import joblib
+    # a = count_down([1, 1, 2, 1, 1, 1, 1, 15])
+    # print(a)
     # print(np.array([['1', 1, 2, 1, 1, 1, 1, 15]]).astype('float'))
     # print(['a', 1, 2, 1, 1, 1, 1, 15])
+    encoder = joblib.load('models/encoder.joblib')
+    input = np.array([['SpiceJet', 'Delhi', 'Afternoon', '2', 'Chennai', 'Economy', '8.5', '0'],
+ ['SpiceJet', 'Delhi', 'Afternoon', '2', 'Chennai', 'Economy', '8.5', '1'],
+ ['SpiceJet', 'Delhi', 'Afternoon', '2', 'Chennai', 'Economy', '8.5', '2']])
+    print(input)
+    res = encoder.transform(input)
+    # print(res)
