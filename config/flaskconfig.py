4b02646c3e5553a116b0c4b081d3e0d00554ce62
alexzhao428 <ruyangzhao2022@u.northwestern.edu>
2022-06-04 23:42:45 -0500

    change quotes, sqlite url

--- config/flaskconfig.py
+++ config/flaskconfig.py
@@ -1,15 +1,13 @@
 import os
 DEBUG = True
-LOGGING_CONFIG = "config/logging/local.conf"
+LOGGING_CONFIG = 'config/logging/local.conf'
 PORT = 5001
-APP_NAME = "flight-price-prediction"
+APP_NAME = 'flight-price-prediction'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-HOST = "0.0.0.0"
+HOST = '0.0.0.0'
 SQLALCHEMY_ECHO = False  # If true, SQL for queries made will be printed
 MAX_ROWS_SHOW = 100
 
 SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
 if SQLALCHEMY_DATABASE_URI is None:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///data/tracks.db'
-
-
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///data/flight.db'
