0932571a0238e287edf497c8da7f2ff2a91afafa
alexzhao428 <ruyangzhao2022@u.northwestern.edu>
2022-06-09 14:16:22 -0500

    Delete irrelevant code

--- config/flaskconfig.py
+++ config/flaskconfig.py
@@ -1,12 +1,11 @@
 import os
 DEBUG = True
 LOGGING_CONFIG = 'config/logging/local.conf'
+HOST = '0.0.0.0'
 PORT = 5001
 APP_NAME = 'flight-price-prediction'
-SQLALCHEMY_TRACK_MODIFICATIONS = True
-HOST = '0.0.0.0'
+SQLALCHEMY_TRACK_MODIFICATIONS = True #If set to True, Flask-SQLAlchemy will track modifications of objects and emit signals.
 SQLALCHEMY_ECHO = False  # If true, SQL for queries made will be printed
-MAX_ROWS_SHOW = 100
 
 SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
 if SQLALCHEMY_DATABASE_URI is None:
